8ce9ad7dcf7134e4b14a5f26ee9bb0b8d928d09e
Jefferson Cando <jeff_can1995@hotmail.es>
2024-11-23 13:02:05 -0500

    fix navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
+import { Navbar, NavbarContent, NavbarItem, Link } from "@nextui-org/react";
 
 export default function Nav() {
     return (
